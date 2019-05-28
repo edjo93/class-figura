eeee2e1f167351abcfae40b94313c500b32039c5
edjo93 <eddas.carrasco@unitec.edu>
2019-05-28 08:19:05 -0600

    constructores agregados

---
+++ Figuras/Cuadrado.h
@@ -0,0 +1,14 @@
+#include"Rectangulo.h"
+
+#ifndef CUADRADO_H
+#define CUADRADO_H
+    class Cuadrado:public Rectangulo{
+        private:
+        public:
+            Cuadrado();
+            Cuadrado(double);
+            
+
+    };
+
+#endif
